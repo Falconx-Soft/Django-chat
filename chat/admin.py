2ec942fdb67606401f9a0aece0ed80ef34f056c6
Tayyab <tayyab91929@gmail.com>
2022-06-05 02:13:32 +0500

    display time

--- chat/admin.py
+++ chat/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import*
 # Register your models here.
 
+class ChatMessage(admin.ModelAdmin):
+    readonly_fields = ('user','room','timestamp','content')
+
 admin.site.register(PrivateChatRoom)
-admin.site.register(RoomChatMessage)
+admin.site.register(RoomChatMessage,ChatMessage)
 admin.site.register(ChatRoomUsers)
